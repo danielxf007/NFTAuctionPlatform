4e4d1600458bb3b32fd1058fdab23cf3aafbf79c
danielxf007 <daniel.higuita007@gmail.com>
2021-12-22 13:22:06 -0500

    added sign-up router

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 //var cookieParser = require('cookie-parser');
 //var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var index_router = require('./routes/index');
+var users_router = require('./routes/users');
+var sign_up_router = require('./routes/sign-up');
 
 var app = express();
 
@@ -19,8 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index_router);
+app.use('/users', users_router);
+app.use('/sign-up', sign_up_router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
