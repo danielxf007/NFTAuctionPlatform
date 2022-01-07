3278b2a04aca1f40f27462d1566bd7c691baf06f
danielxf007 <daniel.higuita007@gmail.com>
2022-01-07 17:10:36 -0500

    greets the user

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log(req.session);
-  res.render('user', {title: 'Hello'+req.session.cookie.user_name});
+  res.render('user', {title: 'Hello '+req.session.user_name});
 });
 
 
